fb490983533f68b5d8a80622b1e06acf0e7b33d3
yejineee <yejineee@gmail.com>
2025-08-08 13:35:18 +0900

    chore: 미사용되는 라이브러리 삭제

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
 import {createElement, Fragment} from 'react'
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
 
 const NEWLINE_REGEX = /\r\n|\r|\n/g;
 export const nl2br = (text: string) =>
